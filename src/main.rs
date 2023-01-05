c4e82ee3c94c2a77990e81a0621f374f88a213c7
KagChi <yonuxbot@gmail.com>
2023-01-05 22:54:49 +0700

    feat: json response

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,31 @@
 extern crate dotenv_codegen;
 
 use rocket::{config::{Config}, get, routes};
+use tokio::time::{Instant, interval_at};
+use rocket::serde::{Serialize, json::Json};
+use rocket::http::{Status, ContentType};
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct IndexMessage<'a> {
+    message: &'a str
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> (Status, (ContentType, Json<IndexMessage<'static>>))  {
+    (Status::Ok, 
+        (ContentType::JSON, 
+            Json(IndexMessage {
+                message: "Hello, world!"
+                }
+            )
+        )
+    )
 }
 
 #[tokio::main]
 async fn main() {
-    tokio::spawn(async {
+    tokio::spawn(async move {
         let port = Result::from(dotenv!("PORT").parse::<u16>())
             .unwrap_or("3000".parse::<u16>().unwrap());
 
@@ -23,4 +39,12 @@ async fn main() {
             Err(e) => println!("Rocket instance failed to launch: {}", e)
         }
     });
+
+    let start = Instant::now();
+    let mut interval = interval_at(start, tokio::time::Duration::from_secs(5));
+
+    loop {
+        interval.tick().await;
+    }
+
 }
