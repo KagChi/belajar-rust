0eaaeb5f59f7f6efc404569646ded5ccf042521e
KagChi <yonuxbot@gmail.com>
2023-01-05 22:18:25 +0700

    feat: initial rest api in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!( "Hello, world!" );
+#[macro_use]
+extern crate dotenv_codegen;
+
+use rocket::{config::{Config}, get, routes};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[tokio::main]
+async fn main() {
+    tokio::spawn(async {
+        let port = Result::from(dotenv!("PORT").parse::<u16>())
+            .unwrap_or("3000".parse::<u16>().unwrap());
+
+        let config = Config::figment()
+            .merge(("port", port)
+        );
+        let rocket_instance = rocket::custom(config).mount("/", routes![index]).launch().await;
+        match rocket_instance {
+            Ok(_) => println!("Rocket instance launched"),
+            Err(e) => println!("Rocket instance failed to launch: {}", e)
+        }
+    });
 }
