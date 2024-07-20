30eb42f6c8511c32de6ea2e27cb8ec1ec84e842f
KagChi <yonuxbot@gmail.com>
2024-07-20 23:40:01 +0700

    fix: remove unused deps

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-extern crate dotenv_codegen;
+use std::env;
+use std::net::IpAddr;
 
-use rocket::{get, routes};
+use rocket::{get, routes, Config};
 use rocket::serde::{Serialize, json::Json};
 use rocket::http::ContentType;
+use dotenv::dotenv;
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
@@ -22,8 +24,25 @@ fn index_route() -> (ContentType, Json<IndexMessage<'static>>)  {
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
     tokio::spawn(async move {
-        let _ = rocket::build().mount("/", routes![index_route])
-            .launch().await;
+        let port: u16 = env::var("PORT")
+            .ok()
+            .and_then(|s| s.parse().ok())
+            .unwrap_or(3000);
+
+        let host: IpAddr = env::var("HOST")
+            .ok()
+            .and_then(|s| s.parse().ok())
+            .unwrap_or("0.0.0.0".parse().expect("Failed to parse default host"));
+        
+        let config = Config::figment()
+            .merge(("port", port))
+            .merge(("host", host));
+
+        let _ = rocket::custom(config)
+            .mount("/", routes![index_route])
+                .launch().await;
     }).await.unwrap();
 }
